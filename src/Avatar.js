d5750b8639145f07c049dc44de80db7e71dd5f69
edoardoaccivile <edoardoaccivile@gmail.com>
2019-03-21 17:34:56 +0100

    aggiunti file Avatar.js e Avatar.css

---
+++ src/Avatar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Avatar.css'
+import PropTypes from 'prop-types'
+
+class Avatar extends React.Component {
+  render() {
+    return (
+      <img
+        alt={this.props.name}
+        src={this.props.imgurl}
+        className={'avatar '+this.props.size}
+        onClick={this.props.onClick}
+        name={this.props.name}
+      />
+    )
+  }
+}
+
+export default Avatar;
+
+Avatar.propTypes = {
+  imgurl: PropTypes.string,
+  size: PropTypes.string,
+  name: PropTypes.string,
+  onClick: PropTypes.func,
+}
