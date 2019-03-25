8fd0cb006d09ed92a50e3a53875aa4577460dd7c
Antonio <pellegrini.antoniow@gmail.com>
2019-03-26 00:42:56 +0100

    branching to anto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import CardList from './components/CardList'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CardList />
+      </div>
+    )
+  }
+}
+
+export default App
