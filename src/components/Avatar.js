0463f50534678880c8ec89fd4f7d3573214c10b1
edoardoaccivile <edoardoaccivile@gmail.com>
2019-03-25 13:00:33 +0100

    css + creata logica componente

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -3,16 +3,30 @@ import './Avatar.css'
 import PropTypes from 'prop-types'
 
 class Avatar extends React.Component {
+  getCapitals(string) {
+    let name = string.split(' ');
+    return name[0][0]+name[name.length-1][0]
+  }
+  
   render() {
-    return (
-      <img
-        alt={this.props.name}
-        src={this.props.imgurl}
-        className={'avatar '+this.props.size}
-        onClick={this.props.onClick}
-        name={this.props.name}
-      />
-    )
+    console.log(this.props)
+    if(this.props.imgurl !== '') {
+      return (
+        <img
+          alt={this.props.name}
+          src={this.props.imgurl}
+          className={'Avatar avatar-'+this.props.size}
+          onClick={this.props.onClick}
+          name={this.props.name}
+        />
+      )
+    } else {
+      return (
+        <div className='Avatar' id='avatar-capitals'>
+          {this.getCapitals(this.props.name)}
+        </div>
+      )
+    }
   }
 }
 
