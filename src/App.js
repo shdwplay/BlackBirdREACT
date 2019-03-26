8a9b4045debcc05d222d2273fddfe6dab666789c
Antonio <pellegrini.antoniow@gmail.com>
2019-03-26 15:26:22 +0100

    added selected card feature

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import CardList from './components/CardList'
 import './App.css'
 
 class App extends Component {
+  state={
+    page:'CardList',
+    activeChat:0,
+  }
+
   render() {
+    switch(this.state.page) {
+      case 'Favourites':
+        break;
+  
+      case 'New':
+        break;
+  
+      default:
+          return <CardList activeChat={this.state.activeChat}/>
+    }
     return (
       <div className="App">
-        <CardList />
+        {this.state.page}
       </div>
     )
   }
