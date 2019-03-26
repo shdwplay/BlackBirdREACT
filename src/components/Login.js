6777659639a1dcdc3754a31528fa67350d57d6d7
Antonio <pellegrini.antoniow@gmail.com>
2019-03-26 18:15:08 +0100

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react'
 import logo from '../assets/logo_welcome.png'
 import './Login.css'
 
+
 class Login extends Component {
     state={
         email:'',
         password:''
+    }
+
+    inputVerification = (name, pw) =>{
+        //check for user in db + return user credentials
+        const credentials= ['pippo','123']
+        if(name===credentials[0] && pw===credentials[1]){
+            console.log('login success')
+            this.setState({
+                email:name,
+                password:pw
+            })
+        }
+        else {
+            console.log('login fail')
+            alert('Invalid email or password')
+        }
     }
 
     render(){
@@ -30,13 +47,19 @@ class Login extends Component {
                             <label className="Login-label" htmlFor="remember-me">Remember Me</label>
                         </div>
                         <div className="Login-form-item Login-form-forgot">
-                            <div className="Login-link">Forgot Password?</div>
+                            <div className="Login-link" 
+                                onClick={()=>{
+                                    console.log('forgot password')
+                                }
+                            }>Forgot Password?</div>
                         </div>
                         <input type="button" className="Login-button" value="Login" 
                             onClick={()=> {
-                                console.log('login')
                                 let inputEmail=document.getElementById('InputEmail')
-                                let inputPassword=document.getElementById('InputEmail')
+                                let inputPassword=document.getElementById('InputPassword')
+                                console.log('email: ' + inputEmail.value)
+                                console.log('password:' + inputPassword.value)
+                                this.inputVerification(inputEmail.value,inputPassword.value)                              
                                 }
                             }/>
                     </div>
