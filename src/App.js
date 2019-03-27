b7fe8a812859851252e77e0aab3688ecc30472ba
Antonio <pellegrini.antoniow@gmail.com>
2019-03-27 12:38:12 +0100

    changing app locally

--- src/App.js
+++ src/App.js
@@ -1,45 +1,50 @@
-import React, { Component } from 'react'
-import CardList from './components/CardList'
-import HeaderChat from './components/HeaderChat'
-import Header from './components/Header'
-import SingleChat from './components/SingleChat'
-import './App.css'
+import React, { Component } from "react";
+import CardList from "./components/CardList";
+import HeaderChat from "./components/HeaderChat";
+import Header from "./components/Header";
+import SingleChat from "./components/SingleChat";
+import "./App.css";
 
-import Login from './components/Login'
+import Login from "./components/Login";
 
 class App extends Component {
-  state={
-    page:'CardList',
-    activeChat:null,
-  }
+  state = {
+    page: "CardList",
+    activeChat: null
+  };
   changePage(pageNumber) {
     this.setState({
-      page:'SingleChat',
-      activeChat: pageNumber,
-    })
+      page: "SingleChat",
+      activeChat: pageNumber
+    });
   }
   render() {
-    return(
-      <div><Login /></div>
-    )
+    return (
+      <div>
+        <Login />
+      </div>
+    );
 
-    switch(this.state.page) {
-      case 'Favourites':
+    switch (this.state.page) {
+      case "Favourites":
         break;
-  
-      case 'New':
+
+      case "New":
         break;
-      
-      case 'SingleChat':
+
+      case "SingleChat":
         return (
           <div className="App">
             <div className="megacontainer">
-              <HeaderChat name={this.state.activeChat[0]} status={this.state.activeChat[1]}/>
+              <HeaderChat
+                name={this.state.activeChat[0]}
+                status={this.state.activeChat[1]}
+              />
               <SingleChat collocutor={this.state.activeChat[0]} />
             </div>
           </div>
-        )
-  
+        );
+
       default:
         return (
           <div className="App">
@@ -47,26 +52,29 @@ class App extends Component {
               <div className="supercontainer">
                 <div className="container">
                   <Header />
-                  <CardList activeChat={this.state.activeChat} changeChat={x=>this.changePage(x)}/>
+                  <CardList
+                    activeChat={this.state.activeChat}
+                    changeChat={x => this.changePage(x)}
+                  />
                 </div>
               </div>
             </div>
           </div>
-        )
+        );
     }
     return (
       <div className="App">
         <div className="megacontainer">
           <div className="supercontainer">
             <div className="container">
-              <HeaderChat /> 
+              <HeaderChat />
               {this.state.page}
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
